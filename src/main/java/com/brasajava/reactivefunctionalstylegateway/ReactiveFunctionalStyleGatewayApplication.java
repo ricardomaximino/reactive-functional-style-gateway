bf7a186bd37b7e5d77b5cc724e0db7876185f932
Ricardo Maximino Gonçalves de Moraes <rmgonsalves@vectoritcgroup.com>
2019-03-31 18:19:59 +0200

    first commit

---
+++ src/main/java/com/brasajava/reactivefunctionalstylegateway/ReactiveFunctionalStyleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.brasajava.reactivefunctionalstylegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFunctionalStyleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFunctionalStyleGatewayApplication.class, args);
+	}
+
+}
